c67c616bb0ad1f983842da477928ab62fc4e3fc8
Jesse Plym <bgu254@myy.haaga-helia.fi>
2023-01-16 20:07:06 +0200

    number guessing done

---
+++ number-guess/App.tsx
@@ -0,0 +1,64 @@
+import { StyleSheet, Text, TextInput, View, Button, Alert } from 'react-native';
+import { useMemo, useState } from 'react';
+
+export default function App() {
+
+  const [msg, setMsg] = useState<string>("Guess a number between 1-100");
+  const [guessedNumber, setGuessedNumber] = useState<string>("");
+  const [totalGuesses, setTotalGuesses] = useState<number>(1);
+  const [games, setGames] = useState<number>(0);
+
+  const randomNumber: number = useMemo(() => {
+    return Math.floor(Math.random() * 100) + 1;
+  }, [games])
+  
+  const handlePress = (): void => {
+    if (Number(guessedNumber) > randomNumber) {
+      setMsg(`Your guess ${guessedNumber} is too high`);
+    } else if (Number(guessedNumber) < randomNumber) {
+      setMsg(`Your guess ${guessedNumber} is too low`);
+    } else {
+      Alert.alert(`You guessed the number in ${totalGuesses} guesses`);
+      setMsg("Guess a number between 1-100");
+      setGames((prev: number) => prev + 1);
+      setTotalGuesses(1);
+      setGuessedNumber("");
+      return;
+    }
+    setTotalGuesses((prevGuesses: number) => prevGuesses + 1);
+  }
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>{msg}</Text>
+      <TextInput
+        style={styles.input}
+        keyboardType="numeric"
+        value={guessedNumber}
+        onChangeText={num => setGuessedNumber(num)}
+      />
+      <Button 
+        title="make guess"
+        onPress={handlePress}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  text: {
+    fontSize: 20
+  },
+  input: {
+    fontSize: 20,
+    borderColor: "gray",
+    borderWidth: 1,
+    width: 50,
+    margin: 25
+  }
+});
